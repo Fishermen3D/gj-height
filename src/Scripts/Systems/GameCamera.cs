ab9e0a6b2088df41b05751741d846c53a1c30d69
Petur Wentzlau <petur@wentzlau.dk>
2026-09-13 13:53:31 +0100

    Changed level layout

--- src/Scripts/Systems/GameCamera.cs
+++ src/Scripts/Systems/GameCamera.cs
@@ -9,11 +9,16 @@ public partial class GameCamera : Camera3D
 
 	float followSpeed = 10.0f;
 
-	Vector3 startPosition = new Vector3(0.0f, 6.4f, 13.8f);
+	//Vector3 startPosition = new Vector3(0.0f, 4.4f, 13.8f);
+	//Vector3 startRotation = new Vector3(0.1f, 0.0f, 0.0f);
+
+	Vector3 startPosition = new Vector3(0.0f, 7.2f, 13.8f);
+	Vector3 startRotation = new Vector3(0.0f, 0.0f, 0.0f);
 
 	public override void _Ready()
 	{
 		GlobalPosition = startPosition;
+		Rotation = startRotation;
 
 		foreach(Node child in GetParent().GetChildren())
 		{
@@ -27,7 +32,7 @@ public partial class GameCamera : Camera3D
 	public override void _Process(double delta)
 	{
 		Vector3 centerPoint = GetCenterPointOfPlayers();
-		Vector3 point = new Vector3(centerPoint.X, 6.4f, 13.8f);
+		Vector3 point = new Vector3(centerPoint.X, startPosition.Y, startPosition.Z);
 		GlobalPosition = GlobalPosition.Lerp(point, (float)delta * followSpeed);
 	}
 
